18b10636c5bb1e1e3be47f6eb9a34a4487089ae8
puzikov <bythetort@gmail.com>
2022-12-12 02:34:31 +0600

    Learned how to imply query parameters

--- books.py
+++ books.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from enum import Enum
 
@@ -12,36 +14,17 @@ BOOKS = {
 }
 
 
-class DirectionName(str, Enum):
-    north = 'North'
-    south = 'South'
-    east = 'East'
-    west = 'West'
-
-
 @app.get('/')
-async def read_all_books():
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
     return BOOKS
 
-@app.get('/books/mybook')
-async def read_favorite_book():
-    return {'book_title': 'My favorite book'}
-
-@app.get('/books/{book_id}')
-async def read_book(book_id: int):
-    return {'book_title': book_id}
-
-@app.get('/directions/{direction_name}')
-async def get_direction(direction_name: DirectionName):
-    if direction_name == DirectionName.north:
-        return {'Direction': direction_name, 'sub': 'Up'}
-    if direction_name == DirectionName.south:
-        return {'Direction': direction_name, 'sub': 'Down'}
-    if direction_name == DirectionName.west:
-        return {'Direction': direction_name, 'sub': 'Left'}
-    return {'Direction': direction_name, 'sub': 'Right'}
-
-
+@app.get('/{book_name}')
+async def read_book(book_name: str):
+    return BOOKS[book_name]
 
 
 
