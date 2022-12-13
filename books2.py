5f56f0eba24b54b4b654501e54e09458e7dc386b
puzikov <bythetort@gmail.com>
2022-12-13 21:47:21 +0600

    Learned to create custom http exceptions. added class NegativeNumberException added func negative_number_exception, which handles NegativeNumberException. It is decorated with app.exception_handler

--- books2.py
+++ books2.py
@@ -1,8 +1,13 @@
 from typing import Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 from uuid import UUID
+from starlette.responses import JSONResponse
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
 
 app = FastAPI()
 
@@ -31,8 +36,20 @@ class Book(BaseModel):
 
 BOOKS =[]
 
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request,
+                                            exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={'message': f'Hey, why do you want {exception.books_to_return} '
+                            f'books? You need to read more!'}
+    )
+
 @app.get('/')
 async def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return and books_to_return <0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if len(BOOKS) < 1:
         create_book_no_api()
     if books_to_return and len(BOOKS) >= books_to_return > 0:
